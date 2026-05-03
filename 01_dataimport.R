a083ca005e1b5833eb148e2854ee0a472519bc4f
Jason Mazique <jcmazique@Jasons-MacBook-Air.local>
2026-05-03 09:27:14 -0400

    capitalization

--- 01_dataimport.R
+++ 01_dataimport.R
@@ -14,82 +14,82 @@ library(assertthat)
 #Campylobacter
 campy <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                     sheet = "CAMP")
-campy$condition <- 'campylobacter'
+campy$condition <- 'Campylobacter'
 
 #Cyclospora
 cycl <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "CYCL")
-cycl$condition <- 'cyclospora'
+cycl$condition <- 'Cyclospora'
 
 #Cryptosporidium
 cryp<- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                   sheet = "CRYP")
-cryp$condition <- 'cryptosporidium'
+cryp$condition <- 'Cryptosporidium'
 
 #EAEC
 eaec <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "EAEC")
-eaec$condition <- 'eaec'
+eaec$condition <- 'EAEC'
 
 #Entamoeba 
 enta <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "ENTA")
-enta$condition <- 'entamoeba'
+enta$condition <- 'Entamoeba'
 
 #EPTP
 eptp <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "EPTP")
-eptp$condition <- 'eptp'
+eptp$condition <- 'EPTP'
 
 #EPAP
 epap <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "EPAP")
-epap$condition <- 'epap'
+epap$condition <- 'EPAP'
 
 #ETLT
 etlt <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "ETLT")
-etlt$condition <- 'etlt'
+etlt$condition <- 'ETLT'
 
 #ETST
 etst <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "ETST")
-etst$condition <- 'etst'
+etst$condition <- 'ETST'
 
 #Giardia
 giar <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "GIAR")
-giar$condition <- 'giardia'
+giar$condition <- 'Giardia'
 
 #Norovirus 
 noro <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "NORO")
-noro$condition <- 'norovirus'
+noro$condition <- 'Norovirus'
 
 #Rotavirus 
 rota <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "ROTA")
-rota$condition <- 'rotavirus'
+rota$condition <- 'Rotavirus'
 
 #Salmonella
 salm <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "SALM")
-salm$condition <- 'salmonella'
+salm$condition <- 'Salmonella'
 
 #Shigella
 shig <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "SHIG")
-shig$condition <- 'shigella'
+shig$condition <- 'Shigella'
 
 #STEC
 stec <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "STEC")
-stec$condition <- 'stec'
+stec$condition <- 'STEC'
 
 #Vibrio Cholera 
 vibr <- read_excel("~/Desktop/PlanEO_ModelBuild/Datasets/2026-02-13_FERG_results_UVA-SOM.xlsx", 
                    sheet = "VIBR")
-vibr$condition <- 'vibrio'
+vibr$condition <- 'Vibrio'
 
 #Reformatting column titles for r_bind
 condition_list <- list(A = campy, B = cryp, C = cycl, D = eaec, E = enta, F = epap, 
@@ -137,3 +137,15 @@ assertthat::assert_that(noNA(combined_condns2$SITE_COUNTRY))
 assertthat::assert_that(noNA(combined_condns2$CASES))
 assertthat::assert_that(noNA(combined_condns2$PREV))
 assertthat::assert_that(noNA(combined_condns2$SE))
+
+##
+
+condition_values <- unique(combined_condns2$CONDITION)
+
+for (val in condition_values) {
+  assign(
+    paste0("conditions_", make.names(val)),
+    combined_condns2[combined_condns2$CONDITION == val, ],
+    envir = .GlobalEnv
+  )
+}
