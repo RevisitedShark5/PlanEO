4dd921210b7344d5094f54fe5c35b129457db439
Jason Mazique <jcmazique@Jasons-MacBook-Air.local>
2026-05-16 20:01:29 -0400

    prevalence derivations

--- 03prevalencederivation.R
+++ 03prevalencederivation.R
@@ -1,9 +1,9 @@
 library(metafor)
 
 
-## 
+## Model 1 - General Prevalence 
 
-#Deriving logit-prevalence and SE for each individual study
+#Deriving logit-prevalence and SE for each individual study - 
 combined_condns3 <- escalc(measure = "PLO",
               xi = CASES, 
               ni = SAMPLES,
@@ -16,9 +16,9 @@ results1 <- combined_condns3 %>%
   group_modify(~ {
     model1 <- rma(yi, vi, data = .x, method = "REML")
     
-    # return a tibble (required)
+    #need to return it to a df
     tibble(
-      estimate = model1$b[1],
+      logitPrev = model1$b[1], 
       se = model1$se,
       pval = model1$pval,
       ci.lb = model1$ci.lb,
@@ -27,4 +27,27 @@ results1 <- combined_condns3 %>%
   })
 
 #Estimated prevalence
-results$prev <- plogis(results$estimate)
+results1$prev <- plogis(results1$logitPrev)
+
+## Model 2 - Incorporation of Syndrome (e.g., "inpatient/outpatient") and Age Range
+
+
+#Random-effects meta-analysis by condition
+results2 <- combined_condns3 %>%
+  group_by(CONDITION, SYNDROME, AGERANGE) %>%
+  group_modify(~ {
+    model2 <- rma(yi, vi, data = .x, method = "REML")
+    
+    #need to return it to a df
+    tibble(
+      logitPrev = model2$b[1], 
+      se = model2$se,
+      pval = model2$pval,
+      ci.lb = model2$ci.lb,
+      ci.ub = model2$ci.ub
+    )
+  })
+
+#Estimated prevalence
+results2$prev <- plogis(results2$logitPrev)
+
